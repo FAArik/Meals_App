8e2fc57c91743d7cea9c3bc3ac91b4287470bacb
FAArik <gasgas132.fa@gmail.com>
2024-07-09 12:30:18 +0300

    10.5 introduction to animations

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,13 +5,37 @@ import 'package:flutter_internals/models/meal.dart';
 import 'package:flutter_internals/screens/meals.dart';
 import 'package:flutter_internals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeails});
 
   final List<Meal> availableMeails;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filtered = availableMeails
+    final filtered = widget.availableMeails
         .where((el) => el.categories.contains(category.id))
         .toList();
     Navigator.of(context).push(
@@ -32,23 +56,31 @@ class CategoriesScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) => Padding(
+            padding:
+                EdgeInsets.only(top: 100 - _animationController.value * 100),
+            child: child,
           ),
-          children: [
-            ///...availableCategories.map((category)=>CategoryGridItem(category: category)).toList(),
-            for (final category in availableCategories)
-              CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context, category);
-                },
-              ),
-          ],
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            children: [
+              ///...availableCategories.map((category)=>CategoryGridItem(category: category)).toList(),
+              for (final category in availableCategories)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ),
+            ],
+          ),
         ),
       ),
     );
